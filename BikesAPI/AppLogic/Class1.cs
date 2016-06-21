eaaac05f234cc4e06e9b7f9ef99bd40b1d010245
rick droogh <rak_keeper@hotmail.com>
2016-06-21 13:14:51 +0200

    Added android and windows applications v0.1

---
+++ BikesAPI/AppLogic/Class1.cs
@@ -0,0 +1,26 @@
+﻿using BikeDataModels;
+using Simple.OData.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppLogic
+{
+    public class Connection
+    {
+        ODataClient client = new ODataClient("http://145.24.222.220/v1/");
+        public IEnumerable<trommel> Trommels;
+
+        public Connection()
+        {
+            Simple.OData.Client.V3Adapter.Reference();
+            Trommels = client.For<trommel>("trommels").Filter(t => true).FindEntriesAsync().Result;
+            /*foreach (var trommel in trommels)
+            {
+                Console.WriteLine(trommel.street);
+
+            }*/
+        }
+    }
+}
---
+++ BikesAPI/WindowsApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppLogic;
+
+namespace WindowsApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Connection connection = new Connection();
+            var trommels = connection.Trommels;
+            foreach (var trommel in trommels)
+            {
+                Console.WriteLine(trommel.street);
+            }
+            Console.Read();
+        }
+    }
+}
