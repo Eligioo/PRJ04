e4a8f53e86b101e997f9257bfca15ef48a71ce1b
rick droogh <rak_keeper@hotmail.com>
2016-06-22 16:46:10 +0200

    Added creating bar, pie and line charts by calling a function.(android app)

--- BikesAPI/AndroidApp/MainActivity.cs
+++ BikesAPI/AndroidApp/MainActivity.cs
@@ -7,7 +7,7 @@ using OxyPlot.Series;
 
 namespace AndroidApp
 {
-    [Activity(Label = "AndroidApp1", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Project4 Groep1", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -18,10 +18,12 @@ namespace AndroidApp
             SetContentView(Resource.Layout.Main);
 
             PlotView view = FindViewById<PlotView>(Resource.Id.plot_view);
-            view.Model = CreatePlotModel();
+            // view.Model = CreateBarModel();
+            // view.Model = createPieChart();
+            view.Model = createLineChart();
         }
 
-        private PlotModel CreatePlotModel()
+        private PlotModel createLineChart()
         {
             var plotModel = new PlotModel { Title = "OxyPlot Demo" };
 
@@ -47,6 +49,57 @@ namespace AndroidApp
 
             return plotModel;
         }
+
+        private PlotModel createPieChart()
+        {
+            PlotModel model = new PlotModel { Title = "Pie Sample1" };
+
+            var seriesP1 = new PieSeries { StrokeThickness = 2.0, InsideLabelPosition = 0.8, AngleSpan = 360, StartAngle = 0 };
+
+            seriesP1.Slices.Add(new PieSlice("Africa", 1030) { IsExploded = false, Fill = OxyColors.PaleVioletRed });
+            seriesP1.Slices.Add(new PieSlice("Americas", 929) { IsExploded = true });
+            seriesP1.Slices.Add(new PieSlice("Asia", 4157) { IsExploded = true });
+            seriesP1.Slices.Add(new PieSlice("Europe", 739) { IsExploded = true });
+            seriesP1.Slices.Add(new PieSlice("Oceania", 35) { IsExploded = true });
+
+            model.Series.Add(seriesP1);
+            return model;
+        }
+
+        private PlotModel CreateBarModel()
+        {
+            var model = new PlotModel
+            {
+                Title = "BarSeries",
+                LegendPlacement = LegendPlacement.Outside,
+                LegendPosition = LegendPosition.BottomCenter,
+                LegendOrientation = LegendOrientation.Horizontal,
+                LegendBorderThickness = 0
+            };
+
+            var s1 = new BarSeries { Title = "Series 1", StrokeColor = OxyColors.Black, StrokeThickness = 1 };
+            s1.Items.Add(new BarItem { Value = 25 });
+            s1.Items.Add(new BarItem { Value = 137 });
+            s1.Items.Add(new BarItem { Value = 18 });
+            s1.Items.Add(new BarItem { Value = 40 });
+
+            /*var s2 = new BarSeries { Title = "Series 2", StrokeColor = OxyColors.Black, StrokeThickness = 1 };
+            s2.Items.Add(new BarItem { Value = 12 });
+            s2.Items.Add(new BarItem { Value = 14 });
+            s2.Items.Add(new BarItem { Value = 120 });
+            s2.Items.Add(new BarItem { Value = 26 });
+            */
+            var categoryAxis = new CategoryAxis { Position = AxisPosition.Left };
+            categoryAxis.Labels.Add("Category A");
+            categoryAxis.Labels.Add("Category B");
+            categoryAxis.Labels.Add("Category C");
+            categoryAxis.Labels.Add("Category D");
+            var valueAxis = new LinearAxis { Position = AxisPosition.Bottom, MinimumPadding = 0, MaximumPadding = 0.06, AbsoluteMinimum = 0 };
+            model.Series.Add(s1);
+            model.Axes.Add(categoryAxis);
+            model.Axes.Add(valueAxis);
+
+            return model;
+        }
     }
 }
-
