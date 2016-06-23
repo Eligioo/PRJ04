3a7f0b4646bb58893148198494178fedd687f1a3
rick droogh <rak_keeper@hotmail.com>
2016-06-23 14:58:33 +0200

    added plotting to master

--- BikesAPI/AndroidApp/MainActivity.cs
+++ BikesAPI/AndroidApp/MainActivity.cs
@@ -1,43 +1,27 @@
-﻿using System;
-using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.App;
 using Android.OS;
-using AppLogic;
-using BikeDataModels;
+using OxyPlot.Xamarin.Android;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System.Collections.Generic;
 
 namespace AndroidApp
 {
-    [Activity(Label = "AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Project4 Groep1", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(bundle);
-
-            /*Connection connection = new Connection();
-
-            var trommels = connection.Trommels;
-
-            string sTrommels = "";
-            foreach (var trommel in trommels)
-            {
-                sTrommels += trommel.street;
-            }*/
+            base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            //Button button = FindViewById<Button>(Resource.Id.MyButton);
+            PlotView view = FindViewById<PlotView>(Resource.Id.plot_view);
 
-            //button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            GraphFactory<int> graphFactory = new GraphFactory<int>();
+            view.Model = graphFactory.createGraph(GraphType.Line, new GraphEffect(), new List<int>());
         }
     }
 }
-
