cff7980cc976b5a90a5845680dfb9c6690e65882
Bob van Schendel <bobyantius@hotmail.com>
2016-06-23 14:41:32 +0200

    commit with test for fragment stuff

--- BikesAPI/AndroidApp/MainActivity.cs
+++ BikesAPI/AndroidApp/MainActivity.cs
@@ -10,34 +10,20 @@ using BikeDataModels;
 
 namespace AndroidApp
 {
-    [Activity(Label = "AndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "AndroidApp", MainLauncher = false, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            /*Connection connection = new Connection();
-
-            var trommels = connection.Trommels;
-
-            string sTrommels = "";
-            foreach (var trommel in trommels)
-            {
-                sTrommels += trommel.street;
-            }*/
-
-            // Set our view from the "main" layout resource
+            
             SetContentView(Resource.Layout.Main);
 
             // Get our button from the layout resource,
             // and attach an event to it
             //Button button = FindViewById<Button>(Resource.Id.MyButton);
 
-            //button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            //button.Click += delegate { Navigation.ToGraph1(this); };
         }
     }
 }
-
