a88a70c2a57734878825660dab48c69b4d829a64
Stefan Koolen <nast3zz@gmail.com>
2016-06-23 10:41:42 +0200

    Added SlidingDrawer for menu

--- BikesAPI/AndroidApp/Resources/Resource.Designer.cs
+++ BikesAPI/AndroidApp/Resources/Resource.Designer.cs
@@ -60,8 +60,26 @@ namespace AndroidApp
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050004
+			public const int MenuButton1 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int MenuButton2 = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int MenuButton3 = 2131034118;
+			
 			// aapt resource value: 0x7f050000
 			public const int MyButton = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int content = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int handle = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int slidingDrawer1 = 2131034115;
 			
 			static Id()
 			{
