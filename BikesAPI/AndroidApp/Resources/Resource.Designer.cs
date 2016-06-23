211d7a0e6a1dab201564863628fad13a83d75c76
Stefan Koolen <nast3zz@gmail.com>
2016-06-23 13:33:27 +0200

    Made it possible to include the menu by making a separate menu-layout file.

--- BikesAPI/AndroidApp/Resources/Resource.Designer.cs
+++ BikesAPI/AndroidApp/Resources/Resource.Designer.cs
@@ -100,6 +100,9 @@ namespace AndroidApp
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
 			
+			// aapt resource value: 0x7f030001
+			public const int menu = 2130903041;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
