37b0ea4f4472d945bc3ef2ef327b8ab75b278f10
Stefan Koolen <nast3zz@gmail.com>
2016-06-27 11:10:18 +0200

    Stacklayout for question 1 with white background

--- XamarinForms/XamarinForms/XamarinForms/Question1.cs
+++ XamarinForms/XamarinForms/XamarinForms/Question1.cs
@@ -15,32 +15,10 @@ namespace XamarinForms
     {
         public Question1()
         {
-            var plotModel = new PlotModel { Title = "Hello, Forms!" };
-            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
-            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Maximum = 10, Minimum = 0 });
-
-            var series1 = new LineSeries
+            Title = "    Question 1";
+            this.Content = new StackLayout
             {
-                MarkerType = MarkerType.Circle,
-                MarkerSize = 4,
-                MarkerStroke = OxyColors.White
-            };
-
-            series1.Points.Add(new DataPoint(0.0, 6.0));
-            series1.Points.Add(new DataPoint(1.4, 2.1));
-            series1.Points.Add(new DataPoint(2.0, 4.2));
-            series1.Points.Add(new DataPoint(3.3, 2.3));
-            series1.Points.Add(new DataPoint(4.7, 7.4));
-            series1.Points.Add(new DataPoint(6.0, 6.2));
-            series1.Points.Add(new DataPoint(8.9, 8.9));
-
-            plotModel.Series.Add(series1);
-
-            Content = new PlotView
-            {
-                Model = plotModel,
-                VerticalOptions = LayoutOptions.Fill,
-                HorizontalOptions = LayoutOptions.Fill,
+                BackgroundColor = Color.White
             };
         }
     }
