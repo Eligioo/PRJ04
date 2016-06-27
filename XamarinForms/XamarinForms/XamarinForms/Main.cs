44432942595c3e7c37337d14092c1f08984b19cc
barld <bakeds@gmail.com>
2016-06-27 09:08:38 +0200

    Xamarin Forms version

---
+++ XamarinForms/XamarinForms/XamarinForms/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamarinForms
+{
+    public class Main : MasterDetailPage
+    {
+        MasterMenu MasterDetailPage;
+        public Main()
+        {
+            MasterDetailPage = new MasterMenu();
+            Master = MasterDetailPage;
+
+            MasterDetailPage.ListView.ItemSelected += OnItemSelected;
+            Detail = new NavigationPage(new StartScreen());
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MasterMenuItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                MasterDetailPage.ListView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
+}
---
+++ XamarinForms/XamarinForms/XamarinForms/StartScreen.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using System;
+
+namespace XamarinForms
+{
+    public class StartScreen : ContentPage
+    {
+        public StartScreen()
+        {
+            Title = "Start Screen";
+            this.Content = new StackLayout { Children = { new Image { Source = Device.OnPlatform<string>(string.Empty, "hr.png", "Assets/hr.png") } } };
+        }
+    }
+}
