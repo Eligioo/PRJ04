b33cf30a008a4ac552e3ded774b39a9e65daf059
Stefan Koolen <nast3zz@gmail.com>
2016-06-27 10:14:56 +0200

    Testing new xamarin

--- XamarinForms/XamarinForms/XamarinForms/StartScreen.cs
+++ XamarinForms/XamarinForms/XamarinForms/StartScreen.cs
@@ -8,7 +8,12 @@ namespace XamarinForms
         public StartScreen()
         {
             Title = "Start Screen";
-            this.Content = new StackLayout { Children = { new Image { Source = Device.OnPlatform<string>(string.Empty, "hr.png", "Assets/hr.png") } } };
+            this.Content = new StackLayout {
+                Padding = new Thickness(50,50,50,50),
+                Children = {
+                    new Image { Source = Device.OnPlatform<string>(string.Empty, "hr.png", "Assets/hr.png") }
+                }
+            };
         }
     }
 }
