90d3019825a1251f3eea8ed72c5acefedf7bc81a
Stefan Koolen <nast3zz@gmail.com>
2016-06-28 14:19:33 +0200

    First version of the geolocation class i made

--- XamarinForms/XamarinForms/XamarinForms/GeoLocation/Geo.cs
+++ XamarinForms/XamarinForms/XamarinForms/GeoLocation/Geo.cs
@@ -11,15 +11,32 @@ namespace Project4.GeoLocation
 {
     public class Geo{
         Geocoder geoCoder = new Geocoder();
-
-        async Task<Plugin.Geolocator.Abstractions.Position> getLocation() {
+        public string Longitude;
+        public string Latitude;
+        public Plugin.Geolocator.Abstractions.Position Position;
+        public async Task<Plugin.Geolocator.Abstractions.Position> GetLocation() {
             try
             {
                 var Position = await CrossGeolocator.Current.GetPositionAsync(10000);
+                this.Longitude = Position.Longitude.ToString().Replace(",", ".");
+                this.Latitude = Position.Latitude.ToString().Replace(",", ".");
+                this.Position = Position;
                 return Position;
             }
             catch {
                 return null;
+            }
+        }
+
+        public async Task<string> GetAddress() {
+            try
+            {
+                var location = new Position(this.Position.Latitude, this.Position.Longitude);
+                var possibleAddresses = await geoCoder.GetAddressesForPositionAsync(location);
+                return possibleAddresses.First().ToString();
+            }
+            catch {
+                return $"{this.Position.Latitude},{this.Position.Longitude}";
             }
         }
     }
