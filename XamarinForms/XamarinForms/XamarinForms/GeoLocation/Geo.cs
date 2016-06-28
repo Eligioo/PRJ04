fb793d59de2a025ba0a47f36a052b9d5f287af05
Stefan Koolen <nast3zz@gmail.com>
2016-06-28 11:48:12 +0200

    GeoLocation class

---
+++ XamarinForms/XamarinForms/XamarinForms/GeoLocation/Geo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Maps;
+using Plugin.Geolocator;
+
+
+namespace Project4.GeoLocation
+{
+    public class Geo{
+        Geocoder geoCoder = new Geocoder();
+
+        async Task<Plugin.Geolocator.Abstractions.Position> getLocation() {
+            try
+            {
+                var Position = await CrossGeolocator.Current.GetPositionAsync(10000);
+                return Position;
+            }
+            catch {
+                return null;
+            }
+        }
+    }
+
+}
--- XamarinForms/XamarinForms/XamarinForms/Properties/Resources.Designer.cs
+++ XamarinForms/XamarinForms/XamarinForms/Properties/Resources.Designer.cs
@@ -23,7 +23,7 @@ namespace Project4.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -37,7 +37,7 @@ namespace Project4.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Project4.Properties.Resources", typeof(Resources).GetTypeInfo().Assembly);
@@ -52,7 +52,7 @@ namespace Project4.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
