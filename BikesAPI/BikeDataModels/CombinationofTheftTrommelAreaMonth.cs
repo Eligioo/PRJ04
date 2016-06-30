908e96b11b9407395b9263f3572380295d156fcb
Bob van Schendel <bobyantius@hotmail.com>
2016-06-30 09:17:01 +0200

    commit specially for barldius bootius

---
+++ BikesAPI/BikeDataModels/CombinationofTheftTrommelAreaMonth.cs
@@ -0,0 +1,11 @@
+﻿namespace BikeDataModels
+{
+    public class CombinationofTheftTrommelAreaMonth
+    {
+        public int Thefts { get; set; }
+        public int Trommels { get; set; }
+        public string Neighbourhood { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+    }
+}
