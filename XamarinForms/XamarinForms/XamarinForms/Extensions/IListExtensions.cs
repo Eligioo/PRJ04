109cf881a29c089fcbefe309929558e7a86e7876
barld <bakeds@gmail.com>
2016-07-02 22:48:58 +0200

    add abstraction layer for caching

---
+++ XamarinForms/XamarinForms/XamarinForms/Extensions/IListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4.Extensions
+{
+    public static class IListExtensions
+    {
+        public static void AddRange<T>(this IList<T> iList, IEnumerable<T> range)
+        {
+            foreach (T item in range)
+                iList.Add(item);
+        }
+    }
+}
---
+++ XamarinForms/XamarinForms/XamarinForms/QuestionLoadPage.cs
@@ -0,0 +1,57 @@
+﻿using BikeDataModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Project4
+{
+    public abstract class QuestionLoadPage<TCache> : ContentPage
+    {
+        private static TCache cache;
+        private static bool loaded;
+
+        protected static TCache Cache => cache;
+
+        protected QuestionLoadPage(string path, bool reload = false)
+        {
+            if(!loaded || reload)
+            {
+                loadData(path);
+                showLoading();
+            }
+            else
+                OnCacheLoaded();
+        }
+
+        private void showLoading()
+        {
+            Content = new ActivityIndicator { HorizontalOptions = LayoutOptions.CenterAndExpand, Color = Color.White, IsVisible = true, IsRunning = true };
+        }
+
+        private async void loadData(string path)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string download = await client.GetStringAsync($"http://145.24.222.220/v2/{path}");
+                    cache = JsonConvert.DeserializeObject<TCache>(download);
+                    loaded = true;
+                    OnCacheLoaded();
+                }
+            }
+            catch
+            {
+                OnCacheLoadedFailed();
+            }
+        }
+
+        protected abstract void OnCacheLoaded();
+        protected virtual void OnCacheLoadedFailed() { }
+    }
+}
