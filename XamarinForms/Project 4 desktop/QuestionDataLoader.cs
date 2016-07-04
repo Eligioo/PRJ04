02f9d40386f88729720cb994f460f0e079811ede
barld <bakeds@gmail.com>
2016-07-04 13:19:45 +0200

    add caching layer

--- XamarinForms/Project 4 desktop/QuestionDataLoader.cs
+++ XamarinForms/Project 4 desktop/QuestionDataLoader.cs
@@ -12,7 +12,11 @@ namespace Project_4_desktop
     {
         public event EventHandler OnLoaded;
         public event EventHandler OnFailed;
-        public TData Data { get; private set; }
+
+        private static TData cache;
+        private static bool cached = false;
+
+        public TData Data => QuestionDataLoader<TData>.cache;
 
         public QuestionDataLoader(string path)
         {
@@ -21,13 +25,21 @@ namespace Project_4_desktop
 
         private async void loadData(string path)
         {
+            if(cached)
+            {
+                // hack otherwise eventhandlers are not atached
+                await Task.Delay(10);
+                OnLoaded?.Invoke(Data, EventArgs.Empty);
+                return;
+            }
             try
             {
                 using (var client = new HttpClient())
                 {
                     var download = await client.GetStringAsync($"http://145.24.222.220/v2/{path}");
-                    Data = await JsonConvert.DeserializeObjectAsync<TData>(download);
+                    cache = JsonConvert.DeserializeObject<TData>(download);
                     OnLoaded?.Invoke(Data, EventArgs.Empty);
+                    cached = true;
                 }
             }
             catch
